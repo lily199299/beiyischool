adf820c0679c6ef27ba2622601b2a212e7f7657c
liuxueling <1148443989@qq.com>
2017-08-05 16:35:53 +0800

    折叠

--- src/components/study/study.vue
+++ src/components/study/study.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>学习</div>
+  <div>
+    <div class="panel-title">
+      <p class="panel-nav" @click="checkChange">基金从业</p>
+    </div>
+    <div class="panel-body">
+      基金从业基金从业基金从业基金从业基金从业基金从业
+      基金从业基金从业基金从业基金从业基金从业基金从业
+      基金从业基金从业基金从业基金从业基金从业基金从业
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  export default {
+    methods: {
+      checkChange () {
+      }
+    }
+  }
 </script>
 
-<style lang="stylus" type="stylesheet/stylus">
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  .panel-title
+      width: 100%
+      height: 46px
+      line-height: 46px
+      background-color: #99CCFF
+      .panel-nav
+        text-align: center
 
 </style>
